506ef4a2dccc55a7d031b215067204f86de8a6ec
Ken Chin-Purcell <ken@chin-purcell.org>
2026-09-10 16:39:50 -0700

    R2 sync

---
+++ sync-r2.sh
@@ -0,0 +1,2 @@
+aws s3 sync R2/ s3://images-kenchinpurcell-com/ --endpoint-url https://07862289b39575b6bc78d149b6a3f28c.r2.cloudflarestorage.com
+aws s3 sync s3://images-kenchinpurcell-com/ R2/ --endpoint-url https://07862289b39575b6bc78d149b6a3f28c.r2.cloudflarestorage.com
